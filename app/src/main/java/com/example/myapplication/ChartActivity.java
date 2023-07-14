0911901fe9ae3f577fc25b4bc61f23b106b81814
epirnay <gizmmkr@gmail.com>
2023-07-14 09:43:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ChartActivity.java
@@ -0,0 +1,197 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+
+public class ChartActivity extends AppCompatActivity {
+
+    // variable for our bar chart
+    BarChart barChart;
+
+    // variable for our bar data set.
+    BarDataSet barDataSet1, barDataSet2;
+
+    Button buttonToSettings;
+
+    // array list for storing entries.
+    ArrayList barEntries;
+
+    // creating a string array for displaying days.
+    String[] days = new String[]{"Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday"};
+    GraphView graphView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chart);
+
+        buttonToSettings = (Button) findViewById(R.id.button_ToSetting);
+
+        // initializing variable for bar chart.
+        graphView = findViewById(R.id.idGraphView);
+
+        buttonToSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChartActivity.this, SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // on below line we are adding data to our graph view.
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+                // on below line we are adding
+                // each point on our x and y axis.
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 9),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6),
+                new DataPoint(7, 1),
+                new DataPoint(8, 2)
+        });
+        graphView.setTitle("My Graph View");
+
+        // on below line we are setting
+        // text color to our graph view.
+        graphView.setTitleColor(com.google.android.material.R.color.design_default_color_error);
+
+        // on below line we are setting
+        // our title text size.
+        graphView.setTitleTextSize(18);
+
+        // on below line we are adding
+        // data series to our graph view.
+        graphView.addSeries(series);
+        barChart = findViewById(R.id.idBarChart);
+
+        // creating a new bar data set.
+        barDataSet1 = new BarDataSet(getBarEntriesOne(), "First Set");
+        barDataSet1.setColor(getApplicationContext().getResources().getColor(com.google.android.material.R.color.cardview_dark_background));
+        barDataSet2 = new BarDataSet(getBarEntriesTwo(), "Second Set");
+        barDataSet2.setColor(Color.BLUE);
+
+        // below line is to add bar data set to our bar data.
+        BarData data = new BarData(barDataSet1, barDataSet2);
+
+        // after adding data to our bar data we
+        // are setting that data to our bar chart.
+        barChart.setData(data);
+
+        // below line is to remove description
+        // label of our bar chart.
+        barChart.getDescription().setEnabled(false);
+
+        // below line is to get x axis
+        // of our bar chart.
+        XAxis xAxis = barChart.getXAxis();
+
+        // below line is to set value formatter to our x-axis and
+        // we are adding our days to our x axis.
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(days));
+
+        // below line is to set center axis
+        // labels to our bar chart.
+        xAxis.setCenterAxisLabels(true);
+
+        // below line is to set position
+        // to our x-axis to bottom.
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        // below line is to set granularity
+        // to our x axis labels.
+        xAxis.setGranularity(1);
+
+        // below line is to enable
+        // granularity to our x axis.
+        xAxis.setGranularityEnabled(true);
+
+        // below line is to make our
+        // bar chart as draggable.
+        barChart.setDragEnabled(true);
+
+        // below line is to make visible
+        // range for our bar chart.
+        barChart.setVisibleXRangeMaximum(3);
+
+        // below line is to add bar
+        // space to our chart.
+        float barSpace = 0.1f;
+
+        // below line is use to add group
+        // spacing to our bar chart.
+        float groupSpace = 0.5f;
+
+        // we are setting width of
+        // bar in below line.
+        data.setBarWidth(0.15f);
+
+        // below line is to set minimum
+        // axis to our chart.
+        barChart.getXAxis().setAxisMinimum(0);
+
+        // below line is to
+        // animate our chart.
+        barChart.animate();
+
+        // below line is to group bars
+        // and add spacing to it.
+        barChart.groupBars(0, groupSpace, barSpace);
+
+        // below line is to invalidate
+        // our bar chart.
+        barChart.invalidate();
+    }
+
+    // array list for first set
+    private ArrayList<BarEntry> getBarEntriesOne() {
+
+        // creating a new array list
+        barEntries = new ArrayList<>();
+
+        // adding new entry to our array list with bar
+        // entry and passing x and y axis value to it.
+        barEntries.add(new BarEntry(1f, 4));
+        barEntries.add(new BarEntry(2f, 6));
+        barEntries.add(new BarEntry(3f, 8));
+        barEntries.add(new BarEntry(4f, 2));
+        barEntries.add(new BarEntry(5f, 4));
+        barEntries.add(new BarEntry(6f, 1));
+        return barEntries;
+    }
+
+    // array list for second set.
+    private ArrayList<BarEntry> getBarEntriesTwo() {
+
+        // creating a new array list
+        barEntries = new ArrayList<>();
+
+        // adding new entry to our array list with bar
+        // entry and passing x and y axis value to it.
+        barEntries.add(new BarEntry(1f, 8));
+        barEntries.add(new BarEntry(2f, 12));
+        barEntries.add(new BarEntry(3f, 4));
+        barEntries.add(new BarEntry(4f, 1));
+        barEntries.add(new BarEntry(5f, 7));
+        barEntries.add(new BarEntry(6f, 3));
+        return barEntries;
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private int progress = 0;
+    Button buttonIncrement;
+    Button buttonDecrement;
+    Button buttonToCharts;
+
+    ProgressBar progressBar;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonDecrement = (Button) findViewById(R.id.button_decr);
+        buttonToCharts = (Button) findViewById(R.id.button_tocharts);
+        buttonIncrement = (Button) findViewById(R.id.button_incr);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        textView = (TextView) findViewById(R.id.text_view_progress);
+
+        buttonToCharts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ChartActivity.class);
+                startActivity(intent);
+            }
+        });
+        // when clicked on buttonIncrement progress is increased by 10%
+        buttonIncrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // if progress is less than or equal
+                // to 90% then only it can be increased
+                if (progress <= 90) {
+                    progress += 10;
+                    updateProgressBar();
+                }
+            }
+        });
+
+        // when clicked on buttonIncrement progress is decreased by 10%
+        buttonDecrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // If progress is greater than
+                // 10% then only it can be decreased
+                if (progress >= 10) {
+                    progress -= 10;
+                    updateProgressBar();
+                }
+            }
+        });
+
+    }
+
+    // updateProgressBar() method sets
+    // the progress of ProgressBar in text
+    private void updateProgressBar() {
+        progressBar.setProgress(progress);
+        textView.setText(String.valueOf(progress));
+    }
+}
